b2e5c3cdfe66940839e81f2c994d4369a2e63ebf
Divye Singh <sdivye92@gmail.com>
2024-01-22 16:04:16 +0530

    refactored to take config and db_connector_builder as arg

--- rag/retriever/parent_child_retriever.py
+++ rag/retriever/parent_child_retriever.py
@@ -5,17 +5,15 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.storage._lc_store import create_kv_docstore
 from langchain.storage import LocalFileStore
+from rag.db_connection_handler import OffMemoryDatabaseConnectorBuilder
 
 class ParentChildRetriever:
-    def __init__(self):
-        config = Config.get_instance()
+    def __init__(self, config: Config, db_connector_builder: OffMemoryDatabaseConnectorBuilder):
         embed_model = self.__get_embedding_model(config)
         fs = LocalFileStore(config.doc_store_path)
         self.embedding_model = embed_model
-        self.chunk_db = self.__get_milvus_vector_store(
-            config.milvus.chunk_collection_name,
-            config.milvus.connection_args
-        )
+        self.chunk_db = db_connector_builder.get_connector(
+            config.milvus.chunk_collection_name)
         # based on https://stackoverflow.com/a/77397998
         self.doc_db = create_kv_docstore(fs)
         self.parent_splitter = RecursiveCharacterTextSplitter(**config.parent_splitter)
@@ -28,10 +26,5 @@ class ParentChildRetriever:
             parent_splitter=self.parent_splitter,
         )
 
-    def __get_embedding_model(self, config):
+    def __get_embedding_model(self, config: Config):
         return HuggingFaceEmbeddings(**config.embedding)
-    
-    def __get_milvus_vector_store(self, collection_name, connection_args):
-        return Milvus(embedding_function=self.embedding_model,
-                   collection_name=collection_name,
-                   connection_args=connection_args)
