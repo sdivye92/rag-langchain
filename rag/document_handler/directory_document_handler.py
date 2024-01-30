099342df77dc52534cabfd73db6f5b1517621b34
Divye Singh <sdivye92@gmail.com>
2024-01-30 18:35:58 +0530

    [Divye] class to combine different file_type_handlers

---
+++ rag/document_handler/directory_document_handler.py
@@ -0,0 +1,34 @@
+import os
+import logging
+from enum import Enum
+from typing import List
+from rag.document_handler import (
+    DocumentHandler,
+    DirectoryDocxHandler,
+    DirectoryPDFHandler,
+    DirectoryPptxHandler
+)
+from rag.config import Config
+from rag.retriever import Retriever
+
+class FileTypes(Enum):
+    pdf: DocumentHandler = DirectoryPDFHandler
+    docx: DocumentHandler = DirectoryDocxHandler
+    pptx: DocumentHandler = DirectoryPptxHandler
+
+class DirectoryDocumentHandler:
+    def __init__(self, dir_path, retriever: Retriever, config: Config):
+        self.dir_path = os.path.abspath(os.path.expanduser(dir_path))
+        self.retriever = retriever
+        self.config = config
+        try:
+            self.file_type_handlers: List[DocumentHandler] = [
+                FileTypes[file_type].value(self.dir_path, self.retriever, self.config)
+                    for file_type in config.supported_file_types
+            ]
+        except Exception as e:
+            logging.exception(e)
+        
+    def update_database(self):
+        for file_type_handler in self.file_type_handlers:
+            file_type_handler.update_database()
