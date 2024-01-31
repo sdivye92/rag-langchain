ba7e362a7d4a77601bcbd4d5f27f8f997b6cb3e9
Divye Singh <sdivye92@gmail.com>
2024-01-31 18:53:07 +0530

    [Divye] moved file_type to class variable and removed unwanted imports

--- rag/document_handler/directory_pdf_handler.py
+++ rag/document_handler/directory_pdf_handler.py
@@ -1,19 +1,14 @@
 import re
-import os
-import logging
-import pandas as pd
-from glob import glob
 from rag.config import Config
 from rag.retriever import Retriever
-from pymilvus import connections, Collection
 from langchain_community.document_loaders import PDFMinerPDFasHTMLLoader
 from langchain_community.document_transformers import Html2TextTransformer
 from ._base_document_handler import DocumentHandler
 
 class DirectoryPDFHandler(DocumentHandler):
+    FILE_TYPE = "pdf"
     def __init__(self, dir_path, retriever: Retriever, config: Config):
         super().__init__(dir_path, retriever, config)
-        self.file_type = "pdf"
     
     def _get_doc_from_file(self, file):
         pdf_html_loader = PDFMinerPDFasHTMLLoader(file)
