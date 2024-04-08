14905cd29e27f07a468c3652e048a177f3a79be4
Divye Singh <sdivye92@gmail.com>
2024-04-08 12:24:51 +0530

    [Divye] added gemini and ollama model support

--- rag/llm/__init__.py
+++ rag/llm/__init__.py
@@ -1,3 +1,5 @@
 from .llm_model_generator import LLMModelGenerator
 from .pipeline_generator import PipelineGenerator
 from .chain import Chain
+from .gemini import Gemini
+from .ollama import OllamaMistral
